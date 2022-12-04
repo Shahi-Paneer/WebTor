ce96d2d5f387622f91b909b9f79859e2c376dbaa
Jaiden Shahi <81540701+Shahi-Paneer@users.noreply.github.com>
2022-12-03 17:39:57 -0800

    Kinda does stuff now

--- index.js
+++ index.js
@@ -1,14 +1,73 @@
-const express = require('express')
-const app = express()
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const path = require('path')
 const port = 3000
+const WebTorrent = require('webtorrent')
+const fs = require('fs')
+
+const client = new WebTorrent()
+app.use(express.static('pages'));
+
+io.on('connection', function (socket) {
+
+    console.log('A user connected');
+
+    socket.on('magnet', function (link, callback) {
 
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+        console.log(link)
+
+        let torrentId = link
+        let files
+        let names = []
+        client.add(torrentId, torrent => {
+            files = torrent.files
+            let length = files.length
+            console.log(files)
+
+            files.forEach(element => {
+                names.push(element.name)
+            });
+
+            socket.emit('files', names, function(data){
+
+                console.log(data)
+                files.forEach(file => {
+                  
+                    if (data.includes(file.name) == true){
+                    const source = file.createReadStream()
+                    const destination = fs.createWriteStream(file.name)
+                    source.on('end', () => {
+                        console.log('file:\t\t', file.name)
+                        // close after all files are saved
+                        length -= 1
+                    }).pipe(destination)
+                }
+                })
+
+            })
+            
+            // Stream each file to the disk
+           /* files.forEach(file => {
+                const source = file.createReadStream()
+                const destination = fs.createWriteStream(file.name)
+                source.on('end', () => {
+                    console.log('file:\t\t', file.name)
+                    // close after all files are saved
+                    length -= 1
+                }).pipe(destination)
+            }) */
+        })
+
+
+    })
+
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
 
+server.listen(port);
--- pages/js/index.js
+++ pages/js/index.js
@@ -1,3 +1,58 @@
 window.onload = async function () {
 
+    let socket = io();
+    let magnetlink
+
+    let poop = ['movie1', 'movie45', 'LMAOOOO']
+
+    document.getElementById("magnetbtn").addEventListener("click", function () {
+        console.log(document.getElementById("magnet").value)
+        magnetlink = document.getElementById("magnet").value
+        socket.emit('magnet', magnetlink, function (files) {
+            console.log(files)
+        })
+    })
+
+    socket.on('files', function (files, callback) {
+        console.log(files)
+
+        files.forEach(e => {
+            var checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+            checkbox.name = "name";
+            checkbox.value = "value";
+            checkbox.id = e;
+
+            var label = document.createElement('label')
+            label.htmlFor = "id";
+            label.appendChild(document.createTextNode(e));
+
+            document.getElementById('box').appendChild(checkbox);
+            document.getElementById('box').appendChild(label);
+        });
+
+        let btn = document.createElement("input");
+        btn.type = "button";
+        btn.value = "Download";
+        btn.id = "download"
+
+        btn.onclick = function () {
+            let select = []
+            files.forEach(e => {
+                console.log(e, document.getElementById(e).checked)
+                if (document.getElementById(e).checked === true) {
+                    select.push(e)
+                }
+
+
+            });
+            console.log(select)
+            callback(select)
+        }
+        document.getElementById("dl").appendChild(btn)
+
+        console.log(document.getElementById(files[0]).checked)
+
+    })
+
 }
